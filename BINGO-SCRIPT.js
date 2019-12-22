e54ea1dbf1a749f79210800a55ee502d82ee52af
Lockett Pundt <kalifornium@hotmail.com>
2019-12-22 09:42:28 -0500

    made the number to 50 instead of 100

--- BINGO-SCRIPT.js
+++ BINGO-SCRIPT.js
@@ -4,7 +4,12 @@
 function bingoNumber() {
     let alphabet = ["B", "I", "N", "G", "O"];
 
-    let x = Math.floor((Math.random() * 100) + 1);
+    let x = Math.floor((Math.random() * 50) + 1);
     let y = Math.floor((Math.random() * 4) + 1);
     return alphabet[y] + x;
+}
+
+function startGame() {
+    alert("Ok here we go!");
+
 }
